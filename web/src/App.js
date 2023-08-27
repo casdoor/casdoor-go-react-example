924d5b190094b7163e1c43463a59ffc2edd64e8f
Usher <1270329076@qq.com>
2023-08-28 00:12:57 +0800

    feat: add casdoor-go-react-sdk-example

---
+++ web/src/App.js
@@ -0,0 +1,48 @@
+// Copyright 2023 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import "./App.css";
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthCallback } from "casdoor-react-sdk";
+import * as Setting from "./Setting";
+import HomePage from "./HomePage";
+
+class App extends React.Component {
+  authCallback = (
+    <AuthCallback
+      sdk={Setting.CasdoorSDK}
+      serverUrl={Setting.ServerUrl}
+      saveTokenFromResponse={(res) => {
+        Setting.setToken(res.data);
+        Setting.goToLink("/");
+      }}
+      isGetTokenSuccessful={(res) => res.status === "ok"}
+    />
+  );
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/callback" element={this.authCallback} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ web/src/HomePage.js
@@ -0,0 +1,103 @@
+// Copyright 2023 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from "react";
+import { isSilentSigninRequired, SilentSignin } from "casdoor-react-sdk";
+import * as Setting from "./Setting";
+import LoginPage from "./LoginPage";
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: undefined,
+    };
+  }
+
+  componentDidMount() {
+    if (Setting.isLoggedIn()) {
+      Setting.getUserinfo().then((res) => {
+        if (res?.status === "ok") {
+          this.setState({
+            account: {
+              username: res.data.displayName,
+              avatar: res.data.avatar,
+            },
+          });
+        } else {
+          Setting.showMessage(res?.status);
+        }
+      });
+    }
+  }
+
+  logout() {
+    Setting.logout();
+    Setting.showMessage("logout successfully");
+    Setting.goToLink("/");
+  }
+
+  render() {
+    if (Setting.isLoggedIn()) {
+      if (this.state.account) {
+        return (
+          <div
+            style={{
+              marginTop: 200,
+              textAlign: "center",
+              alignItems: "center",
+            }}
+          >
+            <img
+              width={100}
+              height={100}
+              src={this.state.account.avatar}
+              alt={this.state.account.username}
+            />
+            <br />
+            <p>{this.state.account.username}</p>
+            <br />
+            <button onClick={this.logout}>Logout</button>
+          </div>
+        );
+      } else {
+        return <p>Loading...</p>;
+      }
+    }
+
+    if (isSilentSigninRequired()) {
+      return (
+        <div
+          style={{ marginTop: 200, textAlign: "center", alignItems: "center" }}
+        >
+          <SilentSignin
+            sdk={Setting.CasdoorSDK}
+            isLoggedIn={Setting.isLoggedIn}
+            handleReceivedSilentSigninSuccessEvent={() => Setting.goToLink("/")}
+            handleReceivedSilentSigninFailureEvent={() => {}}
+          />
+          <p>Logging in...</p>
+        </div>
+      );
+    }
+
+    return (
+      <div style={{ marginTop: 200 }}>
+        <LoginPage />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
